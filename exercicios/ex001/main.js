fa23fe909744ebfee7827b44bf855efe0332d5ee
Lucas Viganó da Silva <lucas.vigano@live.com>
2023-04-18 17:14:08 -0300

    .

--- exercicios/ex001/main.js
+++ exercicios/ex001/main.js
@@ -2,17 +2,17 @@ function tocaSom (idElementoAudio) {
     document.querySelector(idElementoAudio).play();
 }
 
+function escolheSom (e) {
+    tocaSom ('#' + e.id);
+}
+
 const listaDeTeclas = document.querySelectorAll('.tecla')
 
 let contador = 0;
 
 while (contador < listaDeTeclas.length) {
-    listaDeTeclas[contador].onclick = function () {
-        tocaSom ('#som_tecla_pom')
-    }
+    listaDeTeclas[contador].onclick = escolheSom (listaDeTeclas[contador]);
 
     contador = contador + 1;
-
-    console.log(contador)
 }
 
