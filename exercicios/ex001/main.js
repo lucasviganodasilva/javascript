764a959f2ead6361a30e9a58f16f8cda44444923
Lucas Viganó da Silva <lucas.vigano@live.com>
2023-04-24 14:08:24 -0300

    projeto finalizado

--- exercicios/ex001/main.js
+++ exercicios/ex001/main.js
@@ -2,17 +2,16 @@ function tocaSom (idElementoAudio) {
     document.querySelector(idElementoAudio).play();
 }
 
-function escolheSom (e) {
-    tocaSom ('#' + e.id);
-}
-
 const listaDeTeclas = document.querySelectorAll('.tecla')
 
-let contador = 0;
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
-while (contador < listaDeTeclas.length) {
-    listaDeTeclas[contador].onclick = escolheSom (listaDeTeclas[contador]);
+    const tecla = listaDeTeclas[contador]
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`
 
-    contador = contador + 1;
+    tecla.onclick = function () {
+        tocaSom (idAudio);
+    }
 }
 
